ff4d81c8865764e0691a469f3e921949942f55b9
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-01-27 14:36:52 -0300

    :recycle: refactor: mover métodos de menu para a classe Menu

--- src/App.java
+++ src/App.java
@@ -3,58 +3,21 @@ import java.util.Scanner;
 
 public class App {
     private final Acoes acoes;
+    private final Menu menu;
     private final AcoesUsuario acoesUsuario;
 
     public App(String caminhoDoArquivo) {
         this.acoes = new Acoes(caminhoDoArquivo);
+        this.menu = new Menu(this.acoes);
         AcoesTelefone acoesTelefone = new AcoesTelefone(this.acoes);
         this.acoesUsuario = new AcoesUsuario(this.acoes, acoesTelefone);
-    }
-
-    private String montarCabecalho() {
-        String agenda = "AGENDA";
-        int tamanhoMax = 20;
-        int padding = (tamanhoMax - agenda.length()) / 2 - 1;
-
-        String cabecalhoSuperior = String.format("%s%n", "#".repeat(tamanhoMax));
-        String cabecalhoMeio = String.format("%s %s %s%n", "#".repeat(padding), "AGENDA", "#".repeat(padding));
-        return cabecalhoSuperior + cabecalhoMeio + cabecalhoSuperior;
-    }
-
-    private String montarContatos() {
-        String contatosSalvos = String.format("%s Contatos %s%nId | Nome%n", ">".repeat(5), "<".repeat(5));
-        List<Contato> contatos = acoes.getContatosCadastrados();
-        for (Contato contato : contatos) {
-            contatosSalvos += String.format("%d | %s %s%n", contato.getId(), contato.getNome(), contato.getSobrenome());
-        }
-
-        return contatosSalvos;
-    }
-
-    private String montarAcoes() {
-        String cabecalho = String.format("%s %s %s%n", ">".repeat(5), "Menu", "<".repeat(5));
-        String acoes = """
-                1 - Adicionar Contato
-                2 - Remover Contato
-                3 - Editar Contato
-                4 - Sair
-                """;
-
-        return cabecalho + acoes;
-    }
-
-    private void montarMenu() {
-        String cabecalho = montarCabecalho();
-        String contatos = montarContatos();
-        String acoes = montarAcoes();
-        System.out.printf("%s%s%n%n%s%n", cabecalho, contatos, acoes);
     }
 
     public void run() {
         Scanner input = new Scanner(System.in);
         boolean ativo = true;
         while (ativo) {
-            montarMenu();
+            menu.montarMenu();
             System.out.print("Informe a opção que deseja: ");
             int escolhaDoUsuario = input.nextInt();
             switch (escolhaDoUsuario) {
