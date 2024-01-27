ca6979d4fb014dc530bd8266c640efcc78728724
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-01-27 14:24:26 -0300

    :sparkles: feat: persistir remoção de dados no arquivo .txt

--- src/AcoesUsuario.java
+++ src/AcoesUsuario.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
 public class AcoesUsuario {
     private final AcoesTelefone acoesTelefone;
     private final Acoes acoes;
-    
+
     public AcoesUsuario(Acoes acoes, AcoesTelefone acoesTelefone) {
         this.acoes = acoes;
         this.acoesTelefone = acoesTelefone;
     }
+
     public void adicionarNovoContato() {
         boolean contatoValido = false;
         Contato novoContato;
@@ -27,11 +28,13 @@ public class AcoesUsuario {
         }
         System.out.println("Contato salvo com sucesso!");
     }
+
     private boolean validarContato(Contato contato) throws Exception {
         if (contato.getNome() == null)
             throw new Exception("O contato deve receber um nome.");
         return true;
     }
+
     private Contato criarNovoContato() {
         Long idDisponivel = getIdDisponivelContato();
         String nomeContato = setDadosContato("nome");
@@ -39,25 +42,28 @@ public class AcoesUsuario {
         List<Telefone> telefonesContato = acoesTelefone.adicionarNovoTelefone();
         return new Contato(idDisponivel, nomeContato, sobrenomeContato, telefonesContato);
     }
+
     private Long getIdDisponivelContato() {
-        return (acoes.getContatosCadastrados().isEmpty()) ? 1L:
+        return (acoes.getContatosCadastrados().isEmpty()) ? 1L :
                 acoes.getContatosCadastrados()
                         .get(acoes.getContatosCadastrados().size() - 1)
                         .getId() + 1;
     }
+
     private String setDadosContato(String dado) {
         Scanner input = new Scanner(System.in);
         System.out.printf("%nInforme o %s do contato: ", dado);
 
         return input.nextLine();
     }
+
     private String formatarContatoParaSalvar(Contato contato) {
         String telefonesContato = "";
         for (Telefone telefone : contato.getTelefones()) {
             telefonesContato += String.format("| %d | %s%d ", telefone.getId(), telefone.getDdd(), telefone.getNumero());
         }
 
-        return String.format("%d | %s %s %s", contato.getId(), contato.getNome(), contato.getSobrenome(),  telefonesContato);
+        return String.format("%d | %s %s %s", contato.getId(), contato.getNome(), contato.getSobrenome(), telefonesContato);
     }
 
     private void salvarContato(Contato contato) {
@@ -80,10 +86,34 @@ public class AcoesUsuario {
             }
         }
     }
-    public void removerContato(Long idContato){
+
+    private void salvarAlteracoesContatos(List<String> dados) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(acoes.getArquivo()));
+
+            for (String dado : dados) {
+                writer.write(dado);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.printf("Não foi possível salvar o novo contato no arquivo %s%n.", acoes.getArquivo().getName());
+            System.err.println("Por favor, reveja as permissões do arquivo e reinicie a aplicação.");
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Ocorreu um erro inesperado.");
+                }
+            }
+        }
+    }
+
+    private Contato encontarContatoPeloId (Long idContato) {
         Contato contatoInteresse = null;
         for (Contato contato : acoes.getContatosCadastrados()) {
-            if (contato.getId().equals(idContato)){
+            if (contato.getId().equals(idContato)) {
                 contatoInteresse = contato;
                 for (Telefone telefone : contato.getTelefones()) {
                     System.out.println(acoesTelefone.removerTelefone(telefone.getId()));
@@ -91,7 +121,19 @@ public class AcoesUsuario {
                 break;
             }
         }
+        return contatoInteresse;
+    }
+
+    public void removerContato(Long idContato) {
         try {
+            Contato contatoInteresse = this.encontarContatoPeloId(idContato);
+            int indiceContatoInteresse = acoes
+                    .getContatosCadastrados()
+                    .indexOf(contatoInteresse);
+            acoes
+                    .getStringContatosCadastrados()
+                    .remove(indiceContatoInteresse);
+            salvarAlteracoesContatos(acoes.getStringContatosCadastrados());
             System.out.printf("Contato %s %s foi removido com sucesso!%n", contatoInteresse.getNome(), contatoInteresse.getSobrenome());
         } catch (NullPointerException e) {
             System.err.printf("Não foi encontrado qualquer contato com o ID %d.", idContato);
