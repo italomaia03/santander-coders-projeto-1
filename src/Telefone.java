d678e88a8056d51416df9ee97330dda6a91a1490
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-01-27 02:06:20 -0300

    doc: adicionar método toString à classe Telefone

--- src/Telefone.java
+++ src/Telefone.java
@@ -1,14 +1,7 @@
 public class Telefone {
-    private static long contador;
     private Long id;
     private String ddd;
     private Long numero;
-
-    public Telefone(String ddd, Long numero) {
-        this.id = ++contador;
-        this.ddd = ddd;
-        this.numero = numero;
-    }
 
     public Telefone(Long id, String ddd, Long numero) {
         this.id = id;
@@ -20,14 +13,6 @@ public class Telefone {
         return id;
     }
 
-    public static long getContador() {
-        return contador;
-    }
-
-    public static void setContador(long contador) {
-        Telefone.contador = contador;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -35,7 +20,7 @@ public class Telefone {
     public String getDdd() {
         return ddd;
     }
-
+    
     public void setDdd(String ddd) {
         this.ddd = ddd;
     }
@@ -46,5 +31,10 @@ public class Telefone {
 
     public void setNumero(Long numero) {
         this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s) %d", this.ddd, this.numero);
     }
 }
