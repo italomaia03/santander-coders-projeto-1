6656d8a691f6589778df1e03ff8198cdd7ca8bcb
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-01-28 14:32:24 -0300

    :lipstick: feat: integrar o retorno dos feedbacks à interface da aplicação

--- src/App.java
+++ src/App.java
@@ -2,35 +2,35 @@ import java.util.Scanner;
 
 public class App {
     private final Menu menu;
-    private final AcoesUsuario acoesUsuario;
+    private final AcoesContato acoesUsuario;
 
     public App(String caminhoDoArquivo) {
         Acoes acoes = new Acoes(caminhoDoArquivo);
         this.menu = new Menu(acoes);
         AcoesTelefone acoesTelefone = new AcoesTelefone(acoes);
-        this.acoesUsuario = new AcoesUsuario(acoes, acoesTelefone);
+        this.acoesUsuario = new AcoesContato(acoes, acoesTelefone);
     }
 
     public void run() {
         Scanner input = new Scanner(System.in);
         boolean ativo = true;
+        String feedbackMetodo = "";
         while (ativo) {
-            menu.montarMenuPrincipal();
+            menu.montarMenuPrincipal(feedbackMetodo);
             System.out.print("Informe a opção que deseja: ");
             int escolhaDoUsuario = input.nextInt();
             switch (escolhaDoUsuario) {
                 case 1 -> {
                     menu.montarMenuCriarContato();
-                    acoesUsuario.adicionarNovoContato();
+                    feedbackMetodo = acoesUsuario.adicionarNovoContato();
                 }
                 case 2 -> {
                     Long idContato = menu.montarMenuCapturarId("Remover Contato", "contato","remover");
-                    acoesUsuario.removerContato(idContato);
+                    feedbackMetodo = acoesUsuario.removerContato(idContato);
                 }
                 case 3 -> {
                     Long idContato = menu.montarMenuCapturarId("Editar contato","contato", "editar");
-                    acoesUsuario.editarContato(idContato, this.menu);
-
+                    feedbackMetodo = acoesUsuario.editarContato(idContato, this.menu);
                 }
                 case 4 -> {
                     System.out.println("Saindo da aplicação...");
